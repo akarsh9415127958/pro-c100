76a606c958fb7f04f179803c49d5b1b0380f41f3
akarsh9415127958 <85821555+akarsh9415127958@users.noreply.github.com>
2021-07-14 13:59:11 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,27 @@
+class Car(object):
+
+
+  def __init__(self, model, color, company, speed_limit):
+    self.color = color
+    self.company = company
+    self.speed_limit = speed_limit
+    self.model = model
+
+  def start(self):
+    print("started")
+
+  def stop(self):
+    print("stopped")
+
+  def accelarate(self):
+    print("accelarating...")
+    "accelarator functionality here"
+
+  def change_gear(self, gear_type):
+    print("gear changed")
+    " gear related functionality here"
+
+
+audi = Car("A6", "red", "audi", 80)
+
+print(audi.start())
